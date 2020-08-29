2fc5583618f12ba7d4b460a4aea5c4c7e23cb292
Matthias Arndt <marndt@asmsoftware.de>
2020-08-29 13:18:12 +0200

    - initial SNESReader class implementation

---
+++ code/common/snes2db9.h
@@ -0,0 +1,29 @@
+/**
+ * SNES to DB9 Joystick converter
+ *
+ * (c) 2020 by Matthias Arndt <marndt@asmsoftware.de>
+ * http://www.asmsoftware.de/
+ *
+ * The MIT License applies to this software. See COPYING for details.
+ *
+ * @file    snes2db9.h
+ * @brief   public API for the SNEs to DB9 Jostick converter software
+ * @details A implementation uses thus API to integrate the logic.
+ *
+ */
+ 
+#ifndef SNES2DB9_H
+	#define SNES2DB9_H
+	
+	#ifdef _cplusplus
+	extern "C"
+	{
+	#endif
+	
+	
+	
+	#ifdef _cplusplus
+	}
+	#endif
+
+#endif
