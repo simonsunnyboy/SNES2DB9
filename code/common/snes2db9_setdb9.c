d0b6104f09c3e242e62d6c7e89bec9369577c876
Matthias Arndt <marndt@asmsoftware.de>
2020-09-13 17:34:26 +0200

    - implement DB9_SetPins() function

---
+++ code/common/snes2db9_setdb9.c
@@ -0,0 +1,43 @@
+/**
+ * SNES to DB9 Joystick converter
+ *
+ * (c) 2020 by Matthias Arndt <marndt@asmsoftware.de>
+ * http://www.asmsoftware.de/
+ *
+ * The MIT License applies to this software. See COPYING for details.
+ *
+ * @file    snes2db9_setdb9.c
+ * @brief   implements DB9_SetPins
+ * @details The DB9 related output ports are set according to the state. DB9 pins are active low.
+ *
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+#include "snes2db9.h"
+
+static void UpdateDB9Pin(SNES2DB9_SetPinFunc setfunc, SNES2DB9_Pin pin, uint8_t bitmask)
+{
+    if(bitmask != 0)
+    {
+        setfunc(pin, SNES2DB9_PIN_LOW);
+    }
+    else
+    {
+        setfunc(pin, SNES2DB9_PIN_HIGH);
+    }
+}
+
+
+void DB9_SetPins ( uint8_t state, SNES2DB9_SetPinFunc setfunc )
+{
+    assert(setfunc != NULL);
+
+    UpdateDB9Pin(setfunc, DB9_UP,    (state & DB9_BTNMASK_Up));
+    UpdateDB9Pin(setfunc, DB9_DOWN,  (state & DB9_BTNMASK_Down));
+    UpdateDB9Pin(setfunc, DB9_LEFT,  (state & DB9_BTNMASK_Left));
+    UpdateDB9Pin(setfunc, DB9_RIGHT, (state & DB9_BTNMASK_Right));
+    UpdateDB9Pin(setfunc, DB9_FIRE,  (state & DB9_BTNMASK_Fire));
+}
