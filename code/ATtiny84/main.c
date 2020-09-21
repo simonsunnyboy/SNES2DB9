3c837fbcbfbb98692210d26ccf1e7a2ecb3de217
Matthias Arndt <marndt@asmsoftware.de>
2020-09-21 20:21:27 +0200

    - task scheduling flag skeleton added to ATtiny84 implementation

--- code/ATtiny84/main.c
+++ code/ATtiny84/main.c
@@ -8,19 +8,48 @@
  *
  * @file    main.c
  * @brief   implements the Attiny84 implementation of the SNES2DB9 project
- * @details 
+ * @details
  *
  */
 
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
+#include <avr/timer.h>
 
 #include "snes2db9.h"
 
-void main(void)
+#define NR_200US_TICKS_PER_MS (5)          /**< number of 200µs ticks per ms */
+#define DB9_UPDATE_TASK_CYCLE_IN_MS (16)   /**< number of ms for update of DB9 state */
+
+/**
+ * @brief   readiness flags for timed tasks
+ * @details Flags do
+ *          - count forward from associated timer tick for given timing
+ *          - reset to 0 on execution of associated task from main loop
+ */
+typedef struct
 {
-	for(;;)
+	volatile uint8_t reader_update_ready;    /**< reader updates occur with 200µs timer increments */
+	volatile uint8_t db9_update_ready;       /**< DB9 state update occurs with given interval in ms derived from 200µs timer */
+} TaskFlags;
+
+static TaskFlags TaskReadiness;  /**< readiness state of tasks */
+
+int main ( void )
+{
+	for ( ;; )
 	{
+		if ( TaskReadiness.reader_update_ready )
+		{
+			TaskReadiness.reader_update_ready = 0;
+		}
+
+		if ( TaskReadiness.db9_update_ready )
+		{
+			TaskReadiness.db9_update_ready = 0;
+		}
 	}
+
+	return 0;
 }
